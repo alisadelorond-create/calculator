00dcfc2c7975c7f17f176e18779a64fc4ab5e749
F.Sumaya <alisadelorond@gmail.com>
2025-11-19 15:38:29 +0700

    19

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -15,7 +15,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(-1, 1), 0)
 
     def test_subtract(self):
-        self.assertEqual(self.calc.subtract(10, 4), 6)
+        self.assertEqual(self.calc.subtract(10, 4), 8)
         self.assertEqual(self.calc.subtract(5, 8), -3)
 
     def test_multiply(self):
@@ -23,7 +23,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(7, 0), 0)
 
     def test_divide(self):
-        self.assertEqual(self.calc.divide(10, 0), 5)
+        self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertAlmostEqual(self.calc.divide(5, 2), 2.5)
 
     def test_divide_by_zero(self):
